30b9c2cb934d345d27fa94cc4ddf165677e9b479
Air-KS <kevinrogeret@gmail.com>
2024-06-18 18:38:49 +0200

    [ADD] - new file index.js for route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,9 +4,15 @@
 
 import { createRouter, createWebHistory } from 'vue-router';
 import registerPage from '@/views/registerPage.vue';
+import loginPage from '@/views/loginPage';
+import adminPage from '@/views/adminPage';
+import clientPage from '@/views/clientPage';
 
 const routes = [
-  { path: '/', name: 'Register', component: registerPage },
+  { path: '/', name: 'Login', component: loginPage },
+  { path: '/register', name: 'Register', component: registerPage},
+  { path: '/admin', name: 'Admin', component: adminPage },
+  { path: '/clientPage', name: 'Client', component: clientPage }
 ];
 
 const router = createRouter({
