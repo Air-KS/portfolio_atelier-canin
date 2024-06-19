616a5b3b2b6ff8b3a892e867222780eaf13e478e
Air-KS <kevinrogeret@gmail.com>
2024-06-19 19:29:47 +0200

    [Modify] - admin and client page paths to allow access based on roles

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -10,15 +10,18 @@ import registerPage from '@/views/registerPage.vue';
 import loginPage from '@/views/loginPage';
 import adminPage from '@/views/adminPage';
 import clientPage from '@/views/clientPage';
-import profilePage from '@/views/profilePage.vue'
+import profilePage from '@/views/profilePage.vue';
+
+// Importer le middleware d'authentification
+import { authGuard } from '@/middleware/auth.js';
 
 // Définir les routes de l'application
 const routes = [
   { path: '/', name: 'Login', component: loginPage },
   { path: '/register', name: 'Register', component: registerPage },
-  { path: '/admin', name: 'Admin', component: adminPage },
-  { path: '/clientPage', name: 'Client', component: clientPage },
-  { path: '/profile/:id', name: 'ProfilePage', component: profilePage }
+  { path: '/admin', name: 'Admin', component: adminPage, meta: { requiresAuth: true, roles: ['admin', 'responsable'] } },
+  { path: '/clientPage', name: 'Client', component: clientPage, meta: { requiresAuth: true, roles: ['client'] } },
+  { path: '/profile/:id', name: 'ProfilePage', component: profilePage, meta: { requiresAuth: true } }
 ];
 
 // Créer le routeur
@@ -26,5 +29,8 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes // routes: routes en syntaxe ES6
 });
+
+// Utiliser le middleware pour vérifier l'authentification et les droits d'accès
+router.beforeEach(authGuard);
 
 export default router;
