7328de358b7c558644c42d0e7c97a43239abf87b
Air-KS <kevinrogeret@gmail.com>
2024-06-19 04:25:33 +0200

    [Modif] - Add new config

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,16 @@
+const webpack = require('webpack');
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false
+  lintOnSave: false,
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        '__VUE_OPTIONS_API__': JSON.stringify(true),
+        '__VUE_PROD_DEVTOOLS__': JSON.stringify(false),
+        '__VUE_PROD_HYDRATION_MISMATCH_DETAILS__': JSON.stringify(false)
+      })
+    ]
+  }
 });
