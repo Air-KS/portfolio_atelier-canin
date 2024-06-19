d60069838303a43b914546238a174d7b29bff136
Air-KS <kevinrogeret@gmail.com>
2024-06-19 18:24:01 +0200

    [path] - Add new path profilePage

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -10,13 +10,15 @@ import registerPage from '@/views/registerPage.vue';
 import loginPage from '@/views/loginPage';
 import adminPage from '@/views/adminPage';
 import clientPage from '@/views/clientPage';
+import profilePage from '@/views/profilePage.vue'
 
 // Définir les routes de l'application
 const routes = [
   { path: '/', name: 'Login', component: loginPage },
   { path: '/register', name: 'Register', component: registerPage },
   { path: '/admin', name: 'Admin', component: adminPage },
-  { path: '/clientPage', name: 'Client', component: clientPage }
+  { path: '/clientPage', name: 'Client', component: clientPage },
+  { path: '/profile/:id', name: 'ProfilePage', component: profilePage }
 ];
 
 // Créer le routeur
