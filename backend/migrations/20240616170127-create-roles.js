eabc9640b966b35bf11eb13baa5454c20b7fb9c6
Air-KS <kevinrogeret@gmail.com>
2024-06-19 04:09:18 +0200

    [New table] - Add new table role in the database

---
+++ backend/migrations/20240616170127-create-roles.js
@@ -0,0 +1,36 @@
+/*
+  ./backend/migrations/20240616170130-create-roles.js
+*/
+
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Roles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      role: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
+      }
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('Roles');
+  }
+};
