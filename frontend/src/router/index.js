aa892319cd0639ffd7b0a4a22d7c167b273e2131
Air-KS <kevinrogeret@gmail.com>
2024-06-19 15:24:34 +0200

    [Comment] - Adjust comments for better readability

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,22 +2,27 @@
   ./frontend/src/router/index.js
 */
 
+// Importer les fonctions de vue-router
 import { createRouter, createWebHistory } from 'vue-router';
+
+// Importer les composants des pages
 import registerPage from '@/views/registerPage.vue';
 import loginPage from '@/views/loginPage';
 import adminPage from '@/views/adminPage';
 import clientPage from '@/views/clientPage';
 
+// Définir les routes de l'application
 const routes = [
   { path: '/', name: 'Login', component: loginPage },
-  { path: '/register', name: 'Register', component: registerPage},
+  { path: '/register', name: 'Register', component: registerPage },
   { path: '/admin', name: 'Admin', component: adminPage },
   { path: '/clientPage', name: 'Client', component: clientPage }
 ];
 
+// Créer le routeur
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes // routes: routes en syntaxe ES6
 });
 
 export default router;
