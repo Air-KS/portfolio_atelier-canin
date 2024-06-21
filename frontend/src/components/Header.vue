09350f11f7e380f371b2f10652e09e43c05065a8
Air-KS <kevinrogeret@gmail.com>
2024-06-22 00:11:31 +0200

    Optimize Header for and add button logo

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -19,7 +19,9 @@
 						<a href="#" class="nav-menu-link">Location</a>
 					</li>
 					<li class="nav-menu-item" v-if="isAdminOrResponsable">
-						<router-link to="/admin" class="nav-menu-link apply">Dashboard</router-link>
+						<router-link to="/admin" class="nav-menu-link apply">
+							Dashboard
+						</router-link>
 					</li>
 					<li class="nav-menu-item" v-else>
 						<a href="#" class="nav-menu-link apply">Book Now</a>
@@ -27,11 +29,12 @@
 				</ul>
 			</nav>
 			<!-- Section des boutons -->
-			<div class="buttons">
-				<!-- Bouton "Book Now" -->
-				<button class="book-now">Book Now</button>
-				<!-- Profil avec menu déroulant -->
-				<div class="profile" @click="toggleDropdown" ref="profile">
+			<div class="auth-section">
+				<!-- Affichage du texte "Login" ou du profil selon l'état de connexion -->
+				<div v-if="!isLoggedIn">
+					<router-link to="/login" class="login-button">Login</router-link>
+				</div>
+				<div v-else class="profile" @click="toggleDropdown" ref="profile">
 					<img
 						src="@/assets/images/profile-circle.svg"
 						alt="Profile"
@@ -43,55 +46,30 @@
 						class="dropdown-arrow"
 					/>
 					<div class="dropdown-menu" v-if="dropdownOpen">
-						<!-- Affichage du menu si l'utilisateur n'est pas connecté -->
-						<template v-if="!isLoggedIn">
-							<router-link to="/login" class="dropdown-item">
-								<img src="@/assets/images/user-login.svg" alt="Login" />
-								Log in
-								<img
-									src="@/assets/images/arrow-r-grey.png"
-									alt="Arrow Icon"
-									class="menu-arrow-icon"
-								/>
-							</router-link>
-							<div class="dropdown-divider"></div>
-							<router-link to="/register" class="dropdown-item">
-								<img src="@/assets/images/user-add.svg" alt="Sign Up" />
-								Sign Up
-								<img
-									src="@/assets/images/arrow-r-grey.png"
-									alt="Arrow Icon"
-									class="menu-arrow-icon"
-								/>
-							</router-link>
-						</template>
-						<!-- Affichage du menu si l'utilisateur est connecté -->
-						<template v-else>
-							<router-link
-								v-if="user && user.id"
-								:to="{ name: 'Profile', params: { id: user.id } }"
-								@click.native="navigateToProfile"
-								class="dropdown-item"
-							>
-								<img src="@/assets/images/user-profile.svg" alt="Profile" />
-								Profile
-								<img
-									src="@/assets/images/arrow-r-grey.png"
-									alt="Arrow Icon"
-									class="menu-arrow-icon"
-								/>
-							</router-link>
-							<div class="dropdown-divider"></div>
-							<a href="#" class="dropdown-item" @click="logout">
-								<img src="@/assets/images/user-disconnected.svg" alt="Logout" />
-								Log Out
-								<img
-									src="@/assets/images/arrow-r-grey.png"
-									alt="Arrow Icon"
-									class="menu-arrow-icon"
-								/>
-							</a>
-						</template>
+						<router-link
+							v-if="user && user.id"
+							:to="{ name: 'Profile', params: { id: user.id } }"
+							@click.native="navigateToProfile"
+							class="dropdown-item"
+						>
+							<img src="@/assets/images/user-profile.svg" alt="Profile" />
+							Profile
+							<img
+								src="@/assets/images/arrow-r-grey.png"
+								alt="Arrow Icon"
+								class="menu-arrow-icon"
+							/>
+						</router-link>
+						<div class="dropdown-divider"></div>
+						<a href="#" class="dropdown-item" @click="logout">
+							<img src="@/assets/images/user-disconnected.svg" alt="Logout" />
+							Log Out
+							<img
+								src="@/assets/images/arrow-r-grey.png"
+								alt="Arrow Icon"
+								class="menu-arrow-icon"
+							/>
+						</a>
 					</div>
 				</div>
 			</div>
@@ -115,8 +93,11 @@
 
 			// Propriété calculée pour déterminer le texte du lien
 			isAdminOrResponsable() {
-				return this.user && (this.user.role === 'admin' || this.user.role === 'responsable');
-			}
+				return (
+					this.user &&
+					(this.user.role === 'admin' || this.user.role === 'responsable')
+				);
+			},
 		},
 		methods: {
 			toggleDropdown() {
@@ -156,182 +137,190 @@
 </script>
 
 <style scoped>
-	/* Style de l'en-tête */
-	.header {
-		position: fixed;
-		top: 0;
-		left: 0;
-		width: 100%;
-		min-height: 50px;
-		background-color: var(--color-background);
-		border-bottom: 1px solid #ccc;
-		box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-		padding: 10px 0;
-		z-index: 1000;
-	}
+/* Style de l'en-tête */
+.header {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    background-color: var(--color-background);
+    border-bottom: 1px solid #ccc;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    padding: 0;
+    z-index: 1000;
+}
 
-	/* Container principal */
-	.container {
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		max-width: 1200px;
-		margin: 0 auto;
-		padding: 0 20px;
-	}
+/* Container principal */
+.container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    max-width: 1200px;
+    height: 70px; /* Hauteur fixe pour le header */
+    margin: 0 auto;
+    padding: 0 20px;
+}
 
-	/* Style du logo */
-	.logo img {
-		height: 40px;
-	}
+/* Style du logo */
+.logo img {
+    height: 40px;
+}
 
-	/* Menu de navigation */
-	.nav-menu {
-		display: flex;
-		list-style: none;
-		margin: 0;
-		padding: 0;
-	}
+/* Menu de navigation */
+.nav-menu {
+    display: flex;
+    align-items: center;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
 
-	/* Éléments du menu de navigation */
-	.nav-menu .nav-menu-item {
-		font-family: var(--nav-item-font-family);
-		font-weight: var(--nav-item-font-weight);
-		font-size: 1.1rem;
-		padding: 0 20px;
-	}
+/* Éléments du menu de navigation */
+.nav-menu .nav-menu-item {
+    font-family: var(--nav-item-font-family);
+    font-weight: var(--nav-item-font-weight);
+    font-size: 1.1rem;
+    padding: 0 20px;
+    display: flex;
+    align-items: center;
+}
 
-	.nav-menu .nav-menu-item:not(:last-child) {
-		border-right: 3px solid #ccc; /* Séparateur entre les éléments */
-	}
+.nav-menu .nav-menu-item:not(:last-child) {
+    border-right: 3px solid #ccc; /* Séparateur entre les éléments */
+}
 
-	.nav-menu .nav-menu-link {
-		display: block;
-		padding: 0.5rem 0;
-		position: relative;
-		color: var(--color-text-dark);
-		text-decoration: none;
-	}
+.nav-menu .nav-menu-link {
+    display: block;
+    padding: 0.5rem 0;
+    position: relative;
+    color: var(--color-text-dark);
+    text-decoration: none;
+}
 
-	.nav-menu .nav-menu-link:hover {
-		color: var(--nav-color-hover);
-		text-decoration: none;
-	}
+.nav-menu .nav-menu-link:hover {
+    color: var(--nav-color-hover);
+    text-decoration: none;
+}
 
-	.nav-menu .nav-menu-link::before {
-		content: '';
-		position: absolute;
-		bottom: 0;
-		left: 0;
-		width: 0;
-		height: 0.2rem;
-		transition: var(--transition-duration) var(--transition-cubic-bezier);
-	}
+.nav-menu .nav-menu-link::before {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 0;
+    height: 0.2rem;
+    transition: var(--transition-duration) var(--transition-cubic-bezier);
+}
 
-	.nav-menu .nav-menu-item:hover .nav-menu-link::before {
-		background-color: var(--nav-color-link);
-		width: 100%;
-	}
+.nav-menu .nav-menu-item:hover .nav-menu-link::before {
+    background-color: var(--nav-color-link);
+    width: 100%;
+}
 
-	.nav-menu .nav-menu-link.apply {
-		color: red;
-	}
+.nav-menu .nav-menu-link.apply {
+    color: red;
+}
 
-	.nav-menu .nav-menu-link.apply:hover {
-		color: var(--nav-color-hover);
-	}
+.nav-menu .nav-menu-link.apply:hover {
+    color: var(--nav-color-hover);
+}
 
-	.nav-menu .nav-menu-link.apply::before {
-		background-color: var(--nav-color-link);
-	}
+.nav-menu .nav-menu-link.apply::before {
+    background-color: var(--nav-color-link);
+}
 
-	/* Section des boutons */
-	.buttons {
-		display: flex;
-		align-items: center;
-		gap: 10px;
-	}
+/* Section des boutons */
+.auth-section {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+}
 
-	/* Style du bouton "Book Now" */
-	.book-now {
-		background-color: #ff3b30;
-		color: #fff;
-		border: none;
-		padding: 10px 20px;
-		border-radius: 25px;
-		cursor: pointer;
-		font-weight: bold;
-		font-size: 1rem;
-	}
+/* Profil avec menu déroulant */
+.profile {
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100px;
+    height: 50px;
+    border: 1px solid #ccc;
+    border-radius: 20px;
+    cursor: pointer;
+    padding: 0;
+}
 
-	.book-now:hover {
-		background-color: #ff665a;
-	}
+.profile-icon {
+    height: 50px; /* Ajusté pour mieux aligner verticalement */
+    border-radius: 50%;
+    margin-right: 20px;
+}
 
-	/* Profil avec menu déroulant */
-	.profile {
-		position: relative;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 80px;
-		height: 40px;
-		border: 1px solid #ccc;
-		border-radius: 20px;
-		cursor: pointer;
-		padding: 0 0 0 0;
-	}
+.dropdown-arrow {
+    margin-right: 5px;
+    width: 15px;
+    height: 10px;
+}
 
-	.profile-icon {
-		height: 40px;
-		border-radius: 50%;
-		margin-right: 15px;
-	}
+.dropdown-menu {
+    position: absolute;
+    top: 100%; /* Positionné juste en dessous du parent */
+    left: 0;
+    background-color: var(--color-background);
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    border-radius: 5px;
+    overflow: hidden;
+    z-index: 1000;
+    width: 170px; /* Augmenté pour que le texte soit complètement visible */
+}
 
-	.dropdown-arrow {
-		margin-right: 5px;
-		width: 15px;
-		height: 10px;
-	}
+.dropdown-item {
+    display: flex;
+    align-items: center;
+    padding: 10px 20px;
+    color: var(--color-text-dark);
+    text-decoration: none;
+}
 
-	.dropdown-menu {
-		position: absolute;
-		top: 45px;
-		left: 0px;
-		background-color: var(--color-background);
-		box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-		border-radius: 5px;
-		overflow: hidden;
-		z-index: 1000;
-		width: 170px; /* Augmenté pour que le texte soit complètement visible */
-	}
+.dropdown-item:hover {
+    background-color: var(--menu-color-hover);
+}
 
-	.dropdown-item {
-		display: flex;
-		align-items: center;
-		padding: 10px 20px;
-		color: var(--color-text-dark);
-		text-decoration: none;
-	}
+.dropdown-item img {
+    margin-right: 10px;
+    height: 30px;
+}
 
-	.dropdown-item:hover {
-		background-color: var(--menu-color-hover);
-	}
+.menu-arrow-icon {
+    margin-left: auto;
+    height: 20px !important;
+    width: 15px;
+}
 
-	.dropdown-item img {
-		margin-right: 10px;
-		height: 30px;
-	}
+.dropdown-divider {
+    height: 1px;
+    background-color: #4687af;
+    margin: 0 10px;
+}
 
-	.menu-arrow-icon {
-		margin-left: auto;
-		height: 20px !important;
-		width: 15px;
-	}
+.header .login-button {
+    display: inline-block;
+    padding: 10px 20px;
+    border: 2px solid var(--color-button);
+    border-radius: 25px;
+    font-size: 16px;
+    font-weight: bold;
+    color: var(--color-text-dark);
+    text-align: center;
+    text-decoration: none;
+    transition: background-color 0.3s, color 0.3s;
+    font-size: 1.5rem;
+}
 
-	.dropdown-divider {
-		height: 1px;
-		background-color: #4687af;
-		margin: 0 10px;
-	}
+.header .login-button:hover {
+    background-color: var(--menu-color-hover);
+    border: 2px solid #4687af;
+    color: #4687af;
+}
+
 </style>
