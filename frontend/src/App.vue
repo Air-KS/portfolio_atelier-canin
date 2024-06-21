f2e1d5e2845b9c95d17c6751ca90feb41237fe98
Air-KS <kevinrogeret@gmail.com>
2024-06-21 04:05:00 +0200

    add style for padding

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,27 @@
+<!--
+	./frontend/src/App.vue
+-->
+
 <template>
 	<div id="app">
-		<router-view></router-view>
+		<Header />
+		<router-view />
 	</div>
 </template>
 
 <script>
+	import Header from './components/Header.vue';
+
 	export default {
 		name: 'App',
+		components: {
+			Header,
+		},
 	};
 </script>
+
+<style>
+	body {
+		padding-top: 80px;
+	}
+</style>
