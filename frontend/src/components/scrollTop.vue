04a0d46ed830b675dbdaf1d433273ce65ab1875c
Air-KS <kevinrogeret@gmail.com>
2024-06-22 17:58:53 +0200

    [New ScrollTop] - Add scroll to top function on page change

---
+++ frontend/src/components/scrollTop.vue
@@ -0,0 +1,20 @@
+<!--
+	./frontend/src/component/scrollTop.vue
+-->
+
+<template>
+	<div></div>
+</template>
+
+<script>
+export default {
+	name: 'ScrollTop',
+	watch: {
+		$route(to, from) {
+			if (to.path !== from.path) {
+				window.scrollTo(0, 0);
+			}
+		},
+	},
+};
+</script>
