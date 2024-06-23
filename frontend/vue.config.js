208b4550fd83e6412f3b7313fe13f9ce1389aa16
Air-KS <kevinrogeret@gmail.com>
2024-06-24 00:42:48 +0200

    [Update] - Configure webpack

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,9 +1,13 @@
-const webpack = require('webpack');
 const { defineConfig } = require('@vue/cli-service');
+const webpack = require('webpack');
 
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+  publicPath: '/',
+  outputDir: 'dist',
+  assetsDir: 'assets',
+  productionSourceMap: false,
   configureWebpack: {
     output: {
       filename: '[name].[contenthash].js',
@@ -15,6 +19,14 @@ module.exports = defineConfig({
         '__VUE_PROD_DEVTOOLS__': JSON.stringify(false),
         '__VUE_PROD_HYDRATION_MISMATCH_DETAILS__': JSON.stringify(false)
       })
-    ]
+    ],
+    optimization: {
+      splitChunks: {
+        chunks: 'all'
+      }
+    }
+  },
+  devServer: {
+    proxy: 'http://localhost:8080'
   }
 });
