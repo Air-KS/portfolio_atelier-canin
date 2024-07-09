03f801d12f3fdc905aa5f1eb95b1996adb894940
Air-KS <kevinrogeret@gmail.com>
2024-07-09 18:46:28 +0200

    [Create] - Juste create file for the moment

---
+++ frontend/src/components/homePage/homeMiddle.vue
@@ -0,0 +1,17 @@
+<!--
+	./frontend/src/components/homePage/homeDown.vue
+-->
+
+<template>
+	<div class="home-container">
+		<p>Here is the middle of my page.</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'HomeMid',
+};
+</script>
+
+<style src="./homeStyles.css"></style>
