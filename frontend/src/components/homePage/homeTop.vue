768c59adfe3e292860da65fd8313c9dbb623a93f
Air-KS <kevinrogeret@gmail.com>
2024-07-10 19:00:51 +0200

    [New file] - Home Page component, home Page sera en 3 parties

--- frontend/src/components/homePage/homeTop.vue
+++ frontend/src/components/homePage/homeTop.vue
@@ -1,85 +1,229 @@
 <template>
 	<div class="home-container">
 		<div class="home-header">
-			<h1>
-				Welcome Holberton
-				<br />
-				to our Portfolio
-			</h1>
-			<p class="description">
+			<!-- Title and sub title -->
+			<h1 class="welcom">Welcome Holberton</h1>
+			<h2 class="portfolio">Portfolio ATL Canin</h2>
+
+			<!-- description -->
+			<p>
 				We are happy to introduce you today to Atelier Canin, a site
 				specializing in pet grooming services. Come discover our services and
-				our passion for the well-being of your four-legged companions!
+				our passion for the well-being of your four-legged companions !
+			</p>
+
+			<h1>Mobile Pet Grooming</h1>
+			<p>
+				On-Demand, Upfront Pricing, Instant Booking, Same-Day Availability, and
+				Expert Care for All Furry Friends
 			</p>
 		</div>
-		<div class="home-image">
-			<img src="path_to_your_image.png" alt="Cat and Dog" />
+
+		<!-- Comment -->
+		<div class="img-dog">
+			<img src="./../../assets/images/img-dog.png" alt="Cat and Dog" />
 		</div>
-		<button class="button">Book now</button>
-		<div class="already-joined">
-			<img src="path_to_joined_pets_image.png" alt="Joined Pets" />
-			<p>Already 100k+ Pets Joined Us</p>
-			<p>Secure Booking with a Groomer Near You</p>
-		</div>
-		<div class="separator"></div>
+
 		<div class="home-info">
-			<div class="info-section">
-				<h3>Pick it!</h3>
+			<div class="info-section" id="pick">
+				<div class="info">
+					<div class="icon"></div>
+					<h3>Pick it !</h3>
+				</div>
 				<p>Pick from our variety of grooming services for dogs & cats</p>
 			</div>
-			<div class="info-section">
-				<h3>Book it!</h3>
+
+			<div class="separator"></div>
+			<div class="info-section" id="book">
+				<div class="info">
+					<div class="icon"></div>
+					<h3>Book it !</h3>
+				</div>
 				<p>
-					Instantly matched with a professional groomer for an appointment that
-					suits you
+					Instantly get matched with a professional groomer for a date most
+					convenient for you
 				</p>
 			</div>
-			<div class="info-section">
-				<h3>Groom it!</h3>
+
+			<div class="separator"></div>
+			<div class="info-section" id="groom">
+				<div class="info">
+					<div class="icon"></div>
+					<h3>Groom it !</h3>
+				</div>
 				<p>
-					Sit back and relax while we bring the service with all details at your
-					fingertips
+					Sit back and relax as we bring the service to you with all the details
+					at your fingertips
 				</p>
 			</div>
 		</div>
 	</div>
 </template>
 
-  <script>
+<script>
 export default {
 	name: 'HomeTop',
 };
 </script>
 
-  <style src="./homeStyles.css"></style>
-
-  <style scoped>
+<style scoped>
 .home-container {
 	display: flex;
 	flex-direction: column;
 	align-items: center;
-	width: 90%;
-	padding: 20px;
-}
-
-.home-header {
-	text-align: center;
+	justify-content: center;
 	margin-bottom: 20px;
 }
 
-.sub-header {
-	font-size: 1em;
-	color: #555;
+/* Header */
+.home-header {
+	text-align: center;
+	width: 90%;
+}
+
+h1.welcom {
+	font-size: 50px;
+}
+
+h2.portfolio {
+	font-size: 30px;
+	margin-top: -30px;
+}
+
+.info {
+	display: flex;
+	align-items: center;
 	margin-bottom: 10px;
 }
 
-.description {
-	font-size: 1em;
-	margin-bottom: 20px;
+.info .icon {
+	margin-right: 10px;
 }
 
-@media (min-width: 1024px) {
-	.home-container {
+.home-info {
+	display: flex;
+	flex-direction: column;
+	padding: 0px 20px 20px 20px;
+	overflow: hidden;
+	justify-content: left;
+	align-items: left;
+}
+
+.info-section {
+	background: #ffffff;
+	text-align: left;
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	padding: 20px;
+	flex: 1;
+}
+.info-section h3 {
+	margin: 0;
+	font-size: 1.5em;
+	color: var(--color-hover);
+	/* Couleur du texte en rouge */
+}
+
+.info-section p {
+	margin: 10px 0 0;
+	font-size: 1em;
+	color: #666;
+	/* Couleur du texte en gris */
+}
+
+.img-dog {
+	width: 100%;
+	display: flex;
+	justify-content: center;
+	margin: 0 auto;
+}
+
+.img-dog img {
+	width: 95%;
+}
+
+.icon {
+	width: 40px;
+	height: 40px;
+	background-size: contain;
+	background-repeat: no-repeat;
+}
+
+/* Styles spécifiques pour chaque section */
+#pick .icon {
+	background-image: url('./../../assets/images/pick-it.svg'); /* URL de l'icône Pick it */
+}
+#book .icon {
+	background-image: url('./../../assets/images/book-it.svg'); /* URL de l'icône Book it */
+}
+#groom .icon {
+	background-image: url('./../../assets/images/groom-it.svg'); /* URL de l'icône Groom it */
+}
+
+@media (max-width: 599px) {
+	.home-info {
+		gap: 5px;
+	}
+	.info-section {
+		border: 1px solid var(--color-border);
+		border-radius: 0 25px 25px 0;
+		box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+		margin-left: -20px;
+		padding: 10px 10px 10px 20px;
+		width: 98%;
+	}
+}
+
+@media (min-width: 600px) and (max-width: 1024px) {
+	.home-info {
+		border-top: 1px solid var(--color-border);
+		border-bottom: 1px solid var(--color-border);
+		background: white;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+	}
+	.separator {
+		display: flex;
+		width: 1px;
+		height: auto;
+		background-color: var(--color-hover);
+		margin: 20px 5px 0px 5px;
+	}
+	.img-dog img {
+		width: 100%;
+	}
+}
+
+@media (min-width: 1025px) {
+	.home-header {
+		width: 50%;
+	}
+
+	.info-section {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.home-info {
+		border: 1px solid var(--color-border);
+		box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+		border-radius: 25px;
+		background: white;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		width: 70%;
+	}
+	.separator {
+		display: flex;
+		width: 1px;
+		height: auto;
+		background-color: var(--color-hover);
+		margin: 20px 5px 0px 5px;
+	}
+	.img-dog img {
 		width: 70%;
 	}
 }
