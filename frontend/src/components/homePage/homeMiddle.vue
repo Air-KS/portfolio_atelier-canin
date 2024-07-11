7402b3eef00a4b39287eeb00799a07760f9c8d37
Air-KS <kevinrogeret@gmail.com>
2024-07-11 23:58:37 +0200

    [Update] - Update middle page for organization file html and css

--- frontend/src/components/homePage/homeMiddle.vue
+++ frontend/src/components/homePage/homeMiddle.vue
@@ -2,58 +2,95 @@
 	<div class="home-container">
 		<div class="separator"></div>
 		<h2 class="sub-title">Why Atelier Canin ?</h2>
-		<div class="section">
+		<div class="content-container">
 			<!-- Section 1 -->
-			<div class="section-info" id="why1">
-				<h3>Personalized 1 on 1 Sessions</h3>
-				<div class="info">
-					<div class="icon" aria-label="why service icon"></div>
-					<p>
-						Our professionals provide an isolated environment for only your pets
-						where specialized attention to detail is given to all personalized
-						requests.
-					</p>
+			<div class="section">
+				<div class="section-info" id="why1">
+					<h3>Personalized 1 on 1 Sessions</h3>
+					<div class="info">
+						<div class="icon" aria-label="why service icon"></div>
+						<p>
+							Our professionals provide an isolated environment for only your
+							pets where specialized attention to detail is given to all
+							personalized requests.
+						</p>
+					</div>
+				</div>
+				<div class="number">
+					<img
+						src="@/assets/images/homepage/number1.svg"
+						alt="number1"
+						loading="lazy"
+					/>
 				</div>
 			</div>
 
-			<!-- Section 2 -->
-			<div class="section-info" id="why2">
-				<h3>Real Time Updates</h3>
-				<div class="info">
-					<div class="icon" aria-label="why service icon"></div>
-					<p>
-						Atl Canin app puts booking, payment, chatting, communication, support,
-						and grooming services at your fingertips, allowing you to easily connect with groomers,
-						rate their services, and view their profiles all in one place.
-					</p>
+			<!-- Section 1 -->
+			<div class="section">
+				<div class="section-info" id="why2">
+					<h3>Real Time Updates</h3>
+					<div class="info">
+						<div class="icon" aria-label="why service icon"></div>
+						<p>
+							Atl Canin app puts booking, payment, chatting, communication,
+							support, and grooming services at your fingertips, allowing you to
+							easily connect with groomers, rate their services, and view their
+							profiles all in one place.
+						</p>
+					</div>
+				</div>
+				<div class="number">
+					<img
+						src="@/assets/images/homepage/number2.svg"
+						alt="number2"
+						loading="lazy"
+					/>
 				</div>
 			</div>
 
-			<!-- Section 3 -->
-			<div class="section-info" id="why3">
-				<h3>Disponibilité le jour même</h3>
-				<div class="info">
-					<div class="icon" aria-label="why service icon"></div>
-					<p>
-						Pas besoin de planifier à l’avance. Rapide,
-						pratique et parfait pour les horaires chargés.
-						Nous avons ce qu’il vous faut à tout moment et en tout lieu.
-					</p>
+			<!-- Section 1 -->
+			<div class="section">
+				<div class="section-info" id="why3">
+					<h3>Same Day availability</h3>
+					<div class="info">
+						<div class="icon" aria-label="why service icon"></div>
+						<p>
+							No need to plan ahead. Quick, convenient, and perfect for busy
+							schedules. We've got you covered anytime at any place.
+						</p>
+					</div>
+				</div>
+				<div class="number">
+					<img
+						src="@/assets/images/homepage/number3.svg"
+						alt="number3"
+						loading="lazy"
+					/>
 				</div>
 			</div>
 
 			<!-- Section 4 -->
-			<div class="section-info" id="why4">
-				<h3>Produits naturels</h3>
-				<div class="info">
-					<div class="icon" aria-label="why service icon"></div>
-					<p>
-						Nos shampooings sont choisis de manière sélective pour assurer la brillance.
-						Apporter de la commodité s’accompagne également d’une qualité de produit exceptionnelle.
-					</p>
+			<div class="section">
+				<div class="section-info" id="why4">
+					<h3>Natural Products</h3>
+					<div class="info">
+						<div class="icon" aria-label="why service icon"></div>
+						<p>
+							Our shampoos are selectively chosen to ensure brilliance. Bringing
+							convenience also comes with exceptional product quality.
+						</p>
+					</div>
+				</div>
+				<div class="number">
+					<img
+						src="@/assets/images/homepage/number4.svg"
+						alt="number3"
+						loading="lazy"
+					/>
 				</div>
 			</div>
 		</div>
+		<buton class="button">Book Now</buton>
 	</div>
 </template>
 
@@ -70,6 +107,13 @@ export default {
 	align-items: center;
 	justify-content: center;
 	margin-top: 30px;
+}
+
+.button {
+	display: flex;
+	align-items: center;
+	border-radius: 25px;
+	text-shadow: 3px 3px 3px rgba(0, 0, 0, 1);
 }
 
 .separator {
@@ -110,19 +154,20 @@ h2.sub-title {
 	display: flex;
 	align-items: center;
 	margin-bottom: 10px;
-	width: 100%; /* Pour s'assurer que le texte utilise l'espace restant */
+	width: 100%;
 }
 
 /* icon */
 .icon {
-	width: 70px; /* Taille originale de l'icône */
-	height: 70px; /* Taille originale de l'icône */
+	width: 70px;
+	height: 70px;
 	background-size: contain;
 	align-items: center;
 	background-repeat: no-repeat;
 	margin-right: 15px;
 	flex-shrink: 0; /* Pour empêcher l'icône de rétrécir */
 }
+
 #why1 .icon {
 	background-image: url('@/assets/images/homepage/why-atlcanin1.svg');
 }
@@ -148,11 +193,36 @@ p {
 
 @media (min-width: 1025px) {
 	.section {
-		width: 70%;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		width: 90%;
+		margin: 0 auto;
 	}
+
+	.section-info {
+		display: flex;
+		align-items: left;
+		width: 50%;
+		border-radius: 10px;
+	}
+
+	.section-info h3 {
+		font-size: 1.2em;
+		padding: 0;
+		margin-bottom: 10px;
+	}
+
 	.separator {
 		margin-top: 30px;
 		width: 70%;
+	}
+}
+
+/* Cacher le chiffre pour les petits écrans */
+@media (max-width: 1024px) {
+	.number {
+		display: none;
 	}
 }
 </style>
