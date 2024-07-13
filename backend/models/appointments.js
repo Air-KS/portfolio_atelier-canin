e71ece8d96224a485df0caf3109a30d93b78f1c8
Air-KS <kevinrogeret@gmail.com>
2024-07-13 16:14:12 +0200

    [New file] - Create new file model sequelize for Appointement]

---
+++ backend/models/appointments.js
@@ -0,0 +1,82 @@
+/*
+  backend/models/appointments.js
+*/
+
+const { Model, DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  class Appointment extends Model {
+    static associate(models) {
+      Appointment.belongsTo(models.Register, { foreignKey: 'user_id', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
+      Appointment.belongsTo(models.Service, { foreignKey: 'service_id', onDelete: 'SET NULL', onUpdate: 'CASCADE' });
+    }
+  }
+
+  Appointment.init({
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'Register',
+        key: 'id',
+      },
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    service_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'Service',
+        key: 'id',
+      },
+      onDelete: 'SET NULL',
+      onUpdate: 'CASCADE',
+    },
+    appointment_time: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  }, {
+    sequelize,
+    modelName: 'Appointment',
+    tableName: 'Appointments',
+    timestamps: false,
+  });
+
+  return Appointment;
+};
