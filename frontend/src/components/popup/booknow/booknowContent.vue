79a923940aee32af5020273ca8307331a2aa5098
Air-KS <kevinrogeret@gmail.com>
2024-08-07 23:06:14 +0200

    Booknow Content

--- frontend/src/components/popup/booknow/booknowContent.vue
+++ frontend/src/components/popup/booknow/booknowContent.vue
@@ -1,72 +1,74 @@
+<!--
+	./frontend/src/components/popup/booknow/booknowContent.vue
+-->
+
 <template>
 	<div class="calendar-container">
-		<h3>Réserver Maintenant</h3>
-		<p>Contenu spécifique pour la réservation.</p>
-		<div class="pag-container">
-			<img
-				src="@/assets/images/pagination/pag1-hover.svg"
-				alt="number1"
-				loading="lazy"
-				@click="currentPage = 1"
-			/>
-			<img
-				src="@/assets/images/pagination/pag2-hover.svg"
-				alt="number2"
-				loading="lazy"
-				@click="currentPage = 2"
-			/>
-			<img
-				src="@/assets/images/pagination/pag3-hover.svg"
-				alt="number3"
-				loading="lazy"
-				@click="currentPage = 3"
-			/>
-			<img
-				src="@/assets/images/pagination/pag4-hover.svg"
-				alt="number4"
-				loading="lazy"
-				@click="currentPage = 4"
-			/>
-		</div>
-		<CalendarStep
-			v-if="currentPage === 1"
-			:appointment="appointment"
-			@next-step="currentPage = 2"
+	  <h3>Réserver Maintenant</h3>
+	  <p>Contenu spécifique pour la réservation.</p>
+	  <div class="pag-container">
+		<img
+		  v-for="page in pages"
+		  :key="page"
+		  :src="getImageSrc(page)"
+		  :alt="'number' + page"
+		  loading="lazy"
+		  @click="currentPage = page"
 		/>
-		<ServiceStep v-else-if="currentPage === 2" @next-step="currentPage = 3" />
-		<InfoStep
-			v-else-if="currentPage === 3"
-			:appointment="appointment"
-			@next-step="currentPage = 4"
-		/>
-		<PaymentStep v-else-if="currentPage === 4" :appointment="appointment" />
+	  </div>
+	  <CalendarStep
+		v-if="currentPage === 1"
+		:appointment="appointment"
+		@next-step="currentPage = 2"
+	  />
+	  <ServiceStep v-else-if="currentPage === 2" @next-step="currentPage = 3" />
+	  <InfoStep
+		v-else-if="currentPage === 3"
+		:appointment="appointment"
+		@next-step="currentPage = 4"
+	  />
+	  <PaymentStep v-else-if="currentPage === 4" :appointment="appointment" />
 	</div>
-</template>
+  </template>
 
-<script>
-import { ref } from 'vue';
-import CalendarStep from './1CalendarStep.vue';
-import ServiceStep from './2ServiceStep.vue';
-import InfoStep from './3InfoStep.vue';
-import PaymentStep from './4PaymentStep.vue';
+  <script>
+  import { ref } from 'vue';
+  import CalendarStep from './1CalendarStep.vue';
+  import ServiceStep from './2ServiceStep.vue';
+  import InfoStep from './3InfoStep.vue';
+  import PaymentStep from './4PaymentStep.vue';
 
-export default {
+  export default {
 	components: {
-		CalendarStep,
-		ServiceStep,
-		InfoStep,
-		PaymentStep,
+	  CalendarStep,
+	  ServiceStep,
+	  InfoStep,
+	  PaymentStep,
 	},
 	setup() {
-		const currentPage = ref(1);
-		const appointment = ref({
-			first_name: '',
-			last_name: '',
-			email: '',
-			phone: '',
-			appointment_time: '',
-		});
-		return { currentPage, appointment };
+	  const currentPage = ref(1);
+	  const pages = [1, 2, 3, 4];
+	  const appointment = ref({
+		first_name: '',
+		last_name: '',
+		email: '',
+		phone: '',
+		appointment_time: '',
+	  });
+
+	  const getImageSrc = (page) => {
+		return currentPage.value === page
+		  ? require(`@/assets/images/pagination/pag${page}-current.svg`)
+		  : require(`@/assets/images/pagination/pag${page}.svg`);
+	  };
+
+	  return { currentPage, pages, appointment, getImageSrc };
 	},
-};
-</script>
+  };
+  </script>
+
+  <style>
+  .pag-container img {
+	cursor: pointer;
+  }
+  </style>
