1740c825ac0f215d23872fc5206a673802d96038
Air-KS <kevinrogeret@gmail.com>
2024-08-07 16:36:52 +0200

    [ADD] - New file for popup booknow step 4 Payement

---
+++ frontend/src/components/popup/booknow/4PaymentStep.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+	  <p>Méthode de payement</p>
+	  <button @click="submitAppointment">Submit</button>
+	</div>
+  </template>
+
+  <script>
+  export default {
+	props: ['appointment'],
+	setup(props) {
+	  const submitAppointment = () => {
+		fetch('http://localhost:3000/api/appointments', {
+		  method: 'POST',
+		  headers: { 'Content-Type': 'application/json' },
+		  body: JSON.stringify(props.appointment)
+		})
+		  .then(response => response.json())
+		  .then(() => alert('Rendez-vous confirmé et notifications envoyées.'))
+		  .catch(() => alert("Erreur lors de l'envoi des notifications."));
+	  };
+	  return { submitAppointment };
+	}
+  };
+  </script>
