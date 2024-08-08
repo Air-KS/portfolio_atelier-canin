331a93e2e083b301063761d0297669eaf570736b
Air-KS <kevinrogeret@gmail.com>
2024-08-08 02:07:21 +0200

    [Update] - Added user data pre-population in booknowContent.vue. Enhanced appointment data handling based on logged-in user.

--- frontend/src/components/popup/booknow/booknowContent.vue
+++ frontend/src/components/popup/booknow/booknowContent.vue
@@ -32,20 +32,21 @@
   </template>
 
   <script>
-  import { ref } from 'vue';
+  import { ref, watch, onMounted } from 'vue';
   import CalendarStep from './1CalendarStep.vue';
   import ServiceStep from './2ServiceStep.vue';
   import InfoStep from './3InfoStep.vue';
   import PaymentStep from './4PaymentStep.vue';
 
   export default {
+	props: ['isLoggedIn', 'user'],
 	components: {
 	  CalendarStep,
 	  ServiceStep,
 	  InfoStep,
 	  PaymentStep,
 	},
-	setup() {
+	setup(props) {
 	  const currentPage = ref(1);
 	  const pages = [1, 2, 3, 4];
 	  const appointment = ref({
@@ -56,13 +57,44 @@
 		appointment_time: '',
 	  });
 
+	  console.log('User Data in BooknowContent:', props.user);
+
+	  const populateAppointment = () => {
+		if (props.user) {
+		  appointment.value.first_name = props.user.first_name || '';
+		  appointment.value.last_name = props.user.last_name || '';
+		  appointment.value.email = props.user.email || '';
+		  appointment.value.phone = props.user.phone || '';
+		  console.log('Appointment Data:', appointment.value);
+		}
+	  };
+
+	  watch(
+		() => props.user,
+		(newVal) => {
+		  if (newVal) {
+			populateAppointment();
+		  }
+		},
+		{ immediate: true }
+	  );
+
+	  onMounted(() => {
+		populateAppointment();
+	  });
+
 	  const getImageSrc = (page) => {
 		return currentPage.value === page
 		  ? require(`@/assets/images/pagination/pag${page}-current.svg`)
 		  : require(`@/assets/images/pagination/pag${page}.svg`);
 	  };
 
-	  return { currentPage, pages, appointment, getImageSrc };
+	  return {
+		currentPage,
+		pages,
+		appointment,
+		getImageSrc,
+	  };
 	},
   };
   </script>
