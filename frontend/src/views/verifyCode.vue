1daeb57ff6e2dafd529638431ed2274431e7f25e
Air-KS <kevinrogeret@gmail.com>
2024-08-09 18:15:08 +0200

    [Update] - Added withCredentials option to axios request for verification code

--- frontend/src/views/verifyCode.vue
+++ frontend/src/views/verifyCode.vue
@@ -12,48 +12,48 @@
 	</div>
   </template>
 
-  <script>
-  import axios from 'axios';
+<script>
+import axios from 'axios';
 
-  export default {
-	name: 'VerifyCode',
-	data() {
-	  return {
-		verificationCode: '',
-		errorMessage: '',
-		email: this.$route.query.email || '', // Récupère l'email depuis query
-	  };
-	},
-	methods: {
-	  validateInput(event) {
-		const value = event.target.value;
-		this.verificationCode = value.replace(/\D/g, ''); // Garde uniquement les chiffres
-	  },
-	  async verifyCode() {
-		if (this.verificationCode.length === 4) { // Assure-toi que cette longueur est correcte (4 ou 6)
-		  try {
-			const response = await axios.post('http://localhost:3000/api/users/verify', {
-			  email: this.email, // Utilise l'email récupéré
-			  code: this.verificationCode,
-			});
+export default {
+  name: 'VerifyCode',
+  data() {
+    return {
+      verificationCode: '',
+      errorMessage: '',
+      email: this.$route.query.email || '', // Récupère l'email depuis query
+    };
+  },
+  methods: {
+    validateInput(event) {
+      const value = event.target.value;
+      this.verificationCode = value.replace(/\D/g, ''); // Garde uniquement les chiffres
+    },
+    async verifyCode() {
+      if (this.verificationCode.length === 4) { // Assure-toi que cette longueur est correcte (4 ou 6)
+        try {
+          const response = await axios.post('http://localhost:3000/api/users/verify', {
+            email: this.email, // Utilise l'email récupéré
+            code: this.verificationCode,
+		}, { withCredentials: true });
 
-			if (response.status === 200) {
-			  this.$router.push({ name: 'Accueil' });
-			}
-		  } catch (error) {
-			console.error('Erreur lors de la vérification du code:', error); // Log l'erreur complète
-			this.errorMessage = error.response?.data?.error || 'Code de vérification incorrect.';
-		  }
-		} else {
-		  this.errorMessage = 'Le code de vérification doit comporter 4 chiffres.';
-		}
-	  },
-	},
-	mounted() {
-	  console.log('Email reçu:', this.email); // Assure-toi que cet email est correct
-	},
-  };
-  </script>
+          if (response.status === 200) {
+            this.$router.push({ name: 'Accueil' });
+          }
+        } catch (error) {
+          console.error('Erreur lors de la vérification du code:', error); // Log l'erreur complète
+          this.errorMessage = error.response?.data?.error || 'Code de vérification incorrect.';
+        }
+      } else {
+        this.errorMessage = 'Le code de vérification doit comporter 4 chiffres.';
+      }
+    },
+  },
+  mounted() {
+    console.log('Email reçu:', this.email); // Assure-toi que cet email est correct
+  },
+};
+</script>
 
   <style scoped>
   .error-message {
