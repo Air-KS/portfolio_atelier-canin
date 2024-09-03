bd389f71bc3df416729cc6ae852ede29f646abda
Air-KS <kevinrogeret@gmail.com>
2024-09-03 20:05:17 +0200

    [Update]

--- frontend/src/components/researchBar.vue
+++ frontend/src/components/researchBar.vue
@@ -3,66 +3,75 @@
 -->
 
 <template>
-	<input
-		type="text"
+	<div class="search-bar-container">
+	  <input
 		class="research-bar"
-		v-model="localSearchQuery"
+		type="text"
 		:placeholder="placeholder"
-	/>
-</template>
+		v-model="localSearchQuery"
+	  />
+	</div>
+  </template>
 
   <script>
-export default {
+  export default {
 	name: 'ResearchBar',
 	props: {
-		searchQuery: {
-			type: String,
-			default: '',
-		},
-		placeholder: {
-			type: String,
-			default: 'Search...',
-		},
+	  modelValue: {
+		type: String,
+		default: '',
+	  },
+	  placeholder: {
+		type: String,
+		default: 'Search...',
+	  },
 	},
 	data() {
-		return {
-			localSearchQuery: this.searchQuery, // Initialisation de localSearchQuery avec la valeur de searchQuery
-		};
+	  return {
+		localSearchQuery: this.modelValue, // Utiliser la prop modelValue pour initialiser la recherche locale
+	  };
 	},
 	watch: {
-		searchQuery(newVal) {
-			this.localSearchQuery = newVal;
-		},
-		localSearchQuery(newVal) {
-			this.$emit('update:modelValue', newVal);
-		},
+	  modelValue(newVal) {
+		this.localSearchQuery = newVal;
+	  },
+	  localSearchQuery(newVal) {
+		this.$emit('update:modelValue', newVal);
+	  },
 	},
-};
-</script>
+  };
+  </script>
 
   <style scoped>
-.research-bar {
+  .research-bar {
 	border: 1px solid var(--color-border-input);
 	border-radius: 15px;
 	padding: 10px;
 	margin: 10px 0;
-	width: 100%;
-	max-width: 25%;
+	width: 70%;
 	box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 	font-size: 16px;
 	font-family: Arial, sans-serif;
 	background-color: var(--color-background-placeholder);
 	transition: all 0.3s ease;
-}
+  }
 
-.research-bar:focus {
+  .search-bar-container {
+	display: flex;
+	justify-content: center;
+	width: 100%;
+	margin-bottom: 20px;
+  }
+
+  .research-bar:focus {
+	align-items: center;
 	border-color: var(--color-border-input-focus);
 	box-shadow: 0 0 8px var(--color-border-shadow);
 	outline: none; /* Supprime le contour par défaut */
-}
+  }
 
-.research-bar::placeholder {
+  .research-bar::placeholder {
 	color: var(--color-placeholder);
 	font-style: italic;
-}
-</style>
+  }
+  </style>
