99f0ef780cdf06408526d7bfc22e176d3eb289a1
Air-KS <kevinrogeret@gmail.com>
2024-09-05 17:09:34 +0200

    [Update] - Css local for mobile and format document

--- frontend/src/components/popup/booknow/1ServiceStep.vue
+++ frontend/src/components/popup/booknow/1ServiceStep.vue
@@ -4,165 +4,201 @@
 
 <template>
 	<div class="services-container">
-	  <p class="title">Services</p>
-	  <div v-if="services.length">
-		<ul class="services-list">
-		  <!-- Liste des services existants -->
-		  <li v-for="service in services" :key="service.id" class="service-item">
-			<div class="service-header">
-			  <label class="service-label">
-				<input
-				  type="checkbox"
-				  :value="service.id"
-				  v-model="selectedServices"
-				  @change="checkSelection"
-				  class="service-checkbox"
-				/>
-			  </label>
-			  <div class="service-name-price-container">
-				<span class="service-name">{{ service.name }}</span>
-				<span class="service-price">{{ service.price }} €</span>
-			  </div>
-			</div>
-			<p>{{ service.description }}</p>
-		  </li>
+		<p class="title">Services</p>
+		<div v-if="services.length">
+			<ul class="services-list">
+				<!-- Liste des services existants -->
+				<li v-for="service in services" :key="service.id" class="service-item">
+					<div class="service-header">
+						<label class="service-label">
+							<input
+								type="checkbox"
+								:value="service.id"
+								v-model="selectedServices"
+								@change="checkSelection"
+								class="service-checkbox"
+							/>
+						</label>
+						<div class="service-name-price-container">
+							<span class="service-name">{{ service.name }}</span>
+							<span class="service-price">{{ service.price }} €</span>
+						</div>
+					</div>
+					<p>{{ service.description }}</p>
+				</li>
 
-		  <!-- Option pour sélectionner tous les services -->
-		  <li class="service-item">
-			<div class="service-header">
-			  <label class="service-label">
-				<input
-				  type="checkbox"
-				  :value="all"
-				  v-model="allSelected"
-				  @change="handleAllSelect"
-				  class="service-checkbox"
-				/>
-			  </label>
-			  <div class="service-name-price-container">
-				<span class="service-name">Soin Complet - (10% de réduction)</span>
-				<span class="service-price">{{ totalWithDiscount }} €</span>
-			  </div>
-			</div>
-			<p>
-			  Sélectionnez tous les services disponibles pour un soin complet. <br />
-			  Les soins complets comprennent actuellement :
-			</p>
-			<!-- Liste des noms de services pour "Soin Complet" -->
-			<ul class="complete-service-list">
-			  <li v-for="service in services" :key="service.id">{{ service.name }}</li>
+				<!-- Option pour sélectionner tous les services -->
+				<li class="service-item">
+					<div class="service-header">
+						<label class="service-label">
+							<input
+								type="checkbox"
+								:checked="allSelected"
+								v-model="allSelected"
+								@change="handleAllSelect"
+								class="service-checkbox"
+							/>
+						</label>
+						<div class="service-name-price-container">
+							<span class="service-name">Soin Complet (-10%)</span>
+							<span class="service-price">{{ totalWithDiscount }} €</span>
+						</div>
+					</div>
+					<p>
+						Sélectionnez tous les services disponibles pour un soin complet.
+						<br />
+						Les soins complets comprennent actuellement :
+					</p>
+					<!-- Liste des noms de services pour "Soin Complet" -->
+					<ul class="complete-service-list">
+						<li v-for="service in services" :key="service.id">
+							{{ service.name }}
+						</li>
+					</ul>
+				</li>
+
+				<!-- Option pour "Aucun Service" -->
+				<li class="service-item">
+					<div class="service-header">
+						<label class="service-label">
+							<input
+								type="checkbox"
+								value="none"
+								v-model="selectedServices"
+								@change="handleNoneSelection"
+								class="service-checkbox"
+							/>
+							<span class="service-name">Aucun Service</span>
+						</label>
+					</div>
+					<p>Ne sélectionnez aucun service pour cet utilisateur.</p>
+				</li>
 			</ul>
-		  </li>
+		</div>
 
-		  <!-- Option pour "Aucun Service" -->
-		  <li class="service-item">
-			<div class="service-header">
-			  <label class="service-label">
-				<input
-				  type="checkbox"
-				  value="none"
-				  v-model="selectedServices"
-				  @change="handleNoneSelection"
-				  class="service-checkbox"
-				/>
-				<span class="service-name">Aucun Service</span>
-			  </label>
-			</div>
-			<p>Ne sélectionnez aucun service pour cet utilisateur.</p>
-		  </li>
-		</ul>
-	  </div>
-
-	  <button class="button" @click="goToNextStep" :disabled="!isNextEnabled">Next</button>
+		<div class="button-container">
+			<button class="button" @click="goToNextStep" :disabled="!isNextEnabled">
+				Next
+			</button>
+		</div>
 	</div>
-  </template>
+</template>
 
 <script>
 import axios from 'axios';
 import { ref, onMounted, computed } from 'vue';
 
 export default {
-  setup(_, { emit }) {
-    const services = ref([]);
-    const selectedServices = ref([]);
-    const allSelected = ref(false); // Variable pour suivre l'état de sélection "Soin Complet"
+	setup(_, { emit }) {
+		const services = ref([]);
+		const selectedServices = ref(['none']);
+		const allSelected = ref(false); // Variable pour suivre l'état de sélection "Soin Complet"
 
-    // Récupérer les services depuis la base de données
-    const fetchServices = async () => {
-      try {
-        const response = await axios.get('http://localhost:3000/api/services');
-        services.value = response.data;
-      } catch (error) {
-        console.error('Erreur lors de la récupération des services :', error);
-      }
-    };
+		// Récupérer les services depuis la base de données
+		const fetchServices = async () => {
+			try {
+				const response = await axios.get(
+					'http://localhost:3000/api/services'
+				);
+				services.value = response.data;
+			} catch (error) {
+				console.error('Erreur lors de la récupération des services :', error);
+			}
+		};
 
-    // Propriété calculée pour obtenir tous les noms de services
-    const allServiceNames = computed(() => {
-      return services.value.map(service => service.name).join(', ');
-    });
+		// Propriété calculée pour obtenir tous les noms de services
+		const allServiceNames = computed(() => {
+			return services.value.map(service => service.name).join(', ');
+		});
 
-    // Calculer le prix total avec une réduction de 10% si "Soin Complet" est sélectionné
-    const totalWithDiscount = computed(() => {
-      if (allSelected.value) {
-        const total = services.value.reduce((sum, service) => sum + parseFloat(service.price), 0);
-        return (total * 0.9).toFixed(2); // Appliquer une réduction de 10%
-      }
-      return 0;
-    });
+		// Calculer le prix total avec une réduction de 10% si "Soin Complet" est sélectionné
+		const totalWithDiscount = computed(() => {
+			if (allSelected.value) {
+				const total = services.value.reduce(
+					(sum, service) => sum + parseFloat(service.price),
+					0
+				);
+				return (total * 0.9).toFixed(2); // Appliquer une réduction de 10%
+			}
+			return 0;
+		});
 
-    // Gérer la sélection de "Soin Complet"
-    const handleAllSelect = () => {
-      if (allSelected.value) {
-        selectedServices.value = services.value.map(service => service.id);
-      } else {
-        selectedServices.value = [];
-      }
-    };
+		// Gérer la sélection de "Soin Complet"
+		const handleAllSelect = () => {
+			if (allSelected.value) {
+				selectedServices.value = services.value.map(service => service.id);
+				// Supprime 'none' de la sélection
+				selectedServices.value = selectedServices.value.filter(
+					service => service !== 'none'
+				);
+			} else {
+				selectedServices.value = [];
+			}
+		};
 
-    // Vérifie si le bouton Next doit être activé
-    const isNextEnabled = computed(() => selectedServices.value.length > 0);
+		// Vérifie si le bouton Next doit être activé
+		const isNextEnabled = computed(() => selectedServices.value.length > 0);
 
-    // Gérer la sélection de "Aucun service"
-    const handleNoneSelection = () => {
-      if (selectedServices.value.includes('none')) {
-        selectedServices.value = ['none'];
-        allSelected.value = false; // Décocher "Soin Complet" si "Aucun Service" est sélectionné
-      } else if (selectedServices.value.length === 0) {
-        selectedServices.value = [];
-      }
-    };
+		// Gérer la sélection de "Aucun service"
+		const handleNoneSelection = () => {
+			if (selectedServices.value.includes('none')) {
+				selectedServices.value = ['none'];
+				allSelected.value = false; // Décocher "Soin Complet" si "Aucun Service" est sélectionné
+			} else if (selectedServices.value.length === 0) {
+				selectedServices.value = [];
+			}
+		};
 
-    // Vérifier la sélection des services
-    const checkSelection = () => {
-      if (selectedServices.value.includes('none') && selectedServices.value.length > 1) {
-        selectedServices.value = selectedServices.value.filter(service => service !== 'none');
-      }
-    };
+		// Vérifier la sélection des services
+		const checkSelection = () => {
+			if (
+				selectedServices.value.includes('none') &&
+				selectedServices.value.length > 1
+			) {
+				selectedServices.value = selectedServices.value.filter(
+					service => service !== 'none'
+				);
+			}
 
-    // Émission de l'événement pour aller à l'étape suivante avec les services sélectionnés
-    const goToNextStep = () => {
-      const selectedServiceNames = services.value
-        .filter(service => selectedServices.value.includes(service.id))
-        .map(service => service.name);
-      emit('next-step', { selectedServices: selectedServices.value, selectedServiceNames });
-    };
+			// Vérifie si tous les services sont sélectionnés
+			const allServiceIds = services.value.map(service => service.id);
+			const allSelectedManually = allServiceIds.every(id =>
+				selectedServices.value.includes(id)
+			);
 
-    onMounted(fetchServices);
+			if (allSelectedManually) {
+				allSelected.value = true; // Coche "Soin Complet" si tous les articles sont sélectionnés
+			} else {
+				allSelected.value = false; // Décoche "Soin Complet" s'il manque un article
+			}
+		};
 
-    return {
-      services,
-      selectedServices,
-      allSelected,
-      totalWithDiscount,
-      allServiceNames,
-      isNextEnabled,
-      goToNextStep,
-      handleNoneSelection,
-      handleAllSelect,
-      checkSelection,
-    };
-  },
+		// Émission de l'événement pour aller à l'étape suivante avec les services sélectionnés
+		const goToNextStep = () => {
+			const selectedServiceNames = services.value
+				.filter(service => selectedServices.value.includes(service.id))
+				.map(service => service.name);
+			emit('next-step', {
+				selectedServices: selectedServices.value,
+				selectedServiceNames,
+			});
+		};
+
+		onMounted(fetchServices);
+
+		return {
+			services,
+			selectedServices,
+			allSelected,
+			totalWithDiscount,
+			allServiceNames,
+			isNextEnabled,
+			goToNextStep,
+			handleNoneSelection,
+			handleAllSelect,
+			checkSelection,
+		};
+	},
 };
 </script>
 
@@ -175,6 +211,7 @@ export default {
 	border: 1px solid var(--color-border);
 	border-radius: 10px;
 	box-shadow: 0 0 8px var(--color-border-shadow);
+	align-items: flex-start;
 }
 
 .title {
@@ -216,8 +253,8 @@ export default {
 .service-checkbox,
 .checkbox-large {
 	margin: 0 10px 0px 0px;
-	width: 20px; /* Augmente la taille de la case à cocher */
-	height: 20px; /* Augmente la taille de la case à cocher */
+	width: 20px;
+	height: 20px;
 }
 
 .service-name-price-container {
@@ -241,13 +278,23 @@ export default {
 }
 
 .complete-service-list {
-  list-style-type: disc; /* Utilise des puces pour les éléments de la liste */
-  margin-left: 50px; /* Ajoute une marge à gauche pour l'indentation */
-  padding: 0;
+	list-style-type: disc; /* Utilise des puces pour les éléments de la liste */
+	margin-left: 50px;
+	padding: 0;
 }
 
 .complete-service-list li {
-  margin-bottom: 5px; /* Espace entre les éléments de la liste */
+	margin-bottom: 5px; /* Espace entre les éléments de la liste */
+}
+
+.serviceDescription {
+	font-size: 5px;
+}
+
+.button-container {
+	display: flex;
+	justify-content: flex-end; /* Aligner à droite */
+	margin-right: 30px;
 }
 
 .button {
@@ -255,15 +302,61 @@ export default {
 	color: white;
 	border: none;
 	border-radius: 5px;
-	padding: 10px 20px;
 	cursor: pointer;
 	font-size: 16px;
 	transition: background-color 0.3s ease;
-	margin-top: 20px;
 }
 
 .button:disabled {
 	background-color: #cccccc;
 	cursor: not-allowed;
+}
+
+p {
+	font-size: 1rem;
+}
+
+/* Styles pour améliorer l'affichage mobile */
+@media (max-width: 599px) {
+	.services-container {
+		border: none;
+		width: 100%;
+		box-shadow: none;
+		padding: 0;
+		margin: 0;
+		box-sizing: border-box;
+	}
+
+	.services-list {
+		padding: 0;
+		margin-bottom: 30px;
+		font-size: 0.9rem;
+	}
+
+	.service-item {
+		border: solid 1px var(--color-hover);
+		background-color: #ffffff;
+		border-radius: 5px;
+		box-shadow: none;
+		width: 100%;
+		margin: 0 0 30 0;
+		padding: 10px;
+		box-sizing: border-box;
+	}
+	p {
+		font-size: 0.8rem;
+	}
+}
+
+@media (min-width: 600px) and (max-width: 1024px) {
+	.services-container {
+		width: 90%;
+	}
+}
+
+@media (min-width: 1025px) {
+	.services-container {
+		width: 70%;
+	}
 }
 </style>
